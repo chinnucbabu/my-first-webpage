5a8cd838f891e0c8c3978e6a71bdfc5b652600e7
chinnucbabu <iamchinnucb@gmail.com>
2019-08-11 19:58:16 +0530

    new files

---
+++ divisible.php
@@ -0,0 +1,11 @@
+<?php
+$a=19;
+if($a%5==0)
+{
+    echo $a. ' is divisible by 5';
+}
+else
+{
+    echo $a. ' is not divisible by 5';
+}
+?>
---
+++ smallest.php
@@ -0,0 +1,12 @@
+<?php
+$a=7;
+$b=3;
+if($a<$b)
+{
+    echo $a. ' is the smallest number';
+}
+else
+{
+    echo $b. ' is the smallest number';
+}
+?>
